357b6c66d93ebed583c4d6a13b4e2d5fe9438749
FRodriguezC90 <116609909+FRodriguezC90@users.noreply.github.com>
2022-12-03 22:55:27 -0300

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -6,6 +6,7 @@ from Pacientes import Pacientes
 from Profesionales import Profesional
 
 #5 Cuentas de perfil paciente
+<<<<<<< Updated upstream
 pc1=Pacientes('2paciente1','1paciente123', 1,'Matias Gonzalez Perez', 18778932-6)
 pc2=Pacientes('2paciente2','2paciente123', 2,'Carlos Jarpa Lopez', 15774232-1)
 pc3=Pacientes('2paciente3','3paciente123', 3,'Fernando Marquez Letelier', 14776972-7)
@@ -13,6 +14,14 @@ pc4=Pacientes('2paciente4','4paciente123', 4,'Nicolas Ferrada Zabaleta', 1644356
 pc5=Pacientes('2paciente5','5paciente123', 5,'Tomas Ovalle Elgueta', 14112345-8)
 
 
+=======
+pc1=Pacientes('2paciente1','2paciente123', 1,'Matias Gonzalez Perez', 18778932-6)
+pc2=Pacientes('2paciente2','2paciente123', 2,'Carlos Jarpa Lopez', 15774232-1)
+pc3=Pacientes('2paciente3','2paciente123', 3,'Fernando Marquez Letelier', 14776972-7)
+pc4=Pacientes('2ni','nicolas123', 4,'Nicolas Ferrada Zabaleta', 16443567-8)
+pc5=Pacientes('2tomas','tomas123', 5,'Tomas Ovalle Elgueta', 14112345-8)
+print (pc1)
+>>>>>>> Stashed changes
 #5 Cuentas de perfil Profesional
 pr1=Profesional('1doctor1','1doctor123', 6,'Andrea Pizarro Galvez', 19887654-6)
 pr2=Profesional('1doctor2','2doctor123', 7,'Carla Aedo Vielma', 15332882-5)
@@ -23,6 +32,8 @@ pr5=Profesional('1doctor5','5doctor123', 10,'Patricio Galaz Andrade', 18332946-3
 Inicio=getpass("Bienvenido a Clinica Inacap... Presiona una tecla para continuar")
 log = input("Ingrese usuario: ")
 pwd = getpass("Ingrese su contraseña: ")
+
+
 
 
 #db = DataBase(input("Ingrese nombre "),getpass("Ingrese Password "))
