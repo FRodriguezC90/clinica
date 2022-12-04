9c22d63404f78d92cb7721906ba994766624bc84
FRodriguezC90 <116609909+FRodriguezC90@users.noreply.github.com>
2022-12-03 22:54:38 -0300

    cambios menu.py
    cambios usuario y pass iguales a bdatos

--- Menu.py
+++ Menu.py
@@ -6,20 +6,19 @@ from Pacientes import Pacientes
 from Profesionales import Profesional
 
 #5 Cuentas de perfil paciente
-pc1=Pacientes('2mastias','mastias123', 1,'Matias Gonzalez Perez', 18778932-6)
-pc2=Pacientes('2carlos','carlos123', 2,'Carlos Jarpa Lopez', 15774232-1)
-pc3=Pacientes('2fernando','fernando123', 3,'Fernando Marquez Letelier', 14776972-7)
-pc4=Pacientes('2nicolas','nicolas123', 4,'Nicolas Ferrada Zabaleta', 16443567-8)
-pc5=Pacientes('2tomas','tomas123', 5,'Tomas Ovalle Elgueta', 14112345-8)
+pc1=Pacientes('2paciente1','1paciente123', 1,'Matias Gonzalez Perez', 18778932-6)
+pc2=Pacientes('2paciente2','2paciente123', 2,'Carlos Jarpa Lopez', 15774232-1)
+pc3=Pacientes('2paciente3','3paciente123', 3,'Fernando Marquez Letelier', 14776972-7)
+pc4=Pacientes('2paciente4','4paciente123', 4,'Nicolas Ferrada Zabaleta', 16443567-8)
+pc5=Pacientes('2paciente5','5paciente123', 5,'Tomas Ovalle Elgueta', 14112345-8)
 
-print(pc1)
 
 #5 Cuentas de perfil Profesional
-pr1=Profesional('1andrea','andrea123', 6,'Andrea Pizarro Galvez', 19887654-6)
-pr2=Profesional('2carla','carla123', 7,'Carla Aedo Vielma', 15332882-5)
-pr3=Profesional('3mateo','mateo123', 8,'Mateo Lara Villa', 16112325-5)
-pr4=Profesional('4camilo','camilo123', 9,'Camilo Zambrano Lara', 18332946-2)
-pr5=Profesional('5patricio','patricio123', 10,'Patricio Galaz Andrade', 18332946-3)
+pr1=Profesional('1doctor1','1doctor123', 6,'Andrea Pizarro Galvez', 19887654-6)
+pr2=Profesional('1doctor2','2doctor123', 7,'Carla Aedo Vielma', 15332882-5)
+pr3=Profesional('1doctor3','3doctor123', 8,'Mateo Lara Villa', 16112325-5)
+pr4=Profesional('1doctor4','4doctor123', 9,'Camilo Zambrano Lara', 18332946-2)
+pr5=Profesional('1doctor5','5doctor123', 10,'Patricio Galaz Andrade', 18332946-3)
 
 Inicio=getpass("Bienvenido a Clinica Inacap... Presiona una tecla para continuar")
 log = input("Ingrese usuario: ")
